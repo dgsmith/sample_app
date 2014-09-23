3b662da264ca9b5c7e8fc4f23373b58d4c2ff684
Grayson Smith <d.grayson.smith@gmail.com>
2014-09-23 15:31:30 +0000

    Add test cases from exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
   end
   
+  test "signup page" do
+    get signup_path
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+  end
+  
 end
