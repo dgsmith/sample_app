d923a77d9a7080eee8f999667012356e1e28b649
Grayson Smith <d.grayson.smith@gmail.com>
2014-09-25 20:39:04 +0000

    Complete exercises and error/flash tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar"}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explination'
+    assert_select 'div.field_with_errors'
   end
   
   test "valid signup information" do
@@ -25,6 +27,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password_confirmation: password}
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
   
 end
