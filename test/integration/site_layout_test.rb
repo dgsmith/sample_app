74c35fa751d2c0459f1bccb8f9b505eb49756adb
Grayson Smith <d.grayson.smith@gmail.com>
2014-10-15 15:36:51 +0000

    Verify site layout links (exercise 2)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,13 +2,27 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
-  test "layout links" do
+  def setup
+    @user = users(:grayson)
+  end
+  
+  test "layout links for non-logged-in user" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+  end
+  
+  test "layout links for logged-in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
   
   test "signup page" do
